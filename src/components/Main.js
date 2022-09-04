872e50c238dd5890778893822ac9549b1a8e16a0
Ted Pliakas <thodorispliakas@gmail.com>
2022-09-04 22:30:50 +0200

    Updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -58,10 +58,11 @@ class Main extends React.Component {
             Passionate about web technologies and specifically about Web
             Development.
           </p>
+          <p>Solid experience in React and Vue ecosystems.</p>
           <p>
-            Currently working as a Frontend Engineer using Vue and React. In my
-            spare time I develop a full stack application based on MERN Stack
-            (Mongo, Express, React, Node) for my Master Thesis.
+            Currently working as a Frontend Engineer in travel industry. In my spare time I dive into data science, python, WebXR, three.js, microfrontends.
+            I'm passionate and always highly interested in new web technologies and I constantly keep learning and studying.
+
           </p>
           <p>
             I'm always highly interested in new web technologies and I
